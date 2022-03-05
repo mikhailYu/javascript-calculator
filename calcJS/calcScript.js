4f51fda580c42ee56d155fe3a844490735ef6057
Mikhail <mikhailyu@hotmail.co.nz>
2022-03-05 15:42:45 +1300

    created the operator functions which run as expected.

---
+++ calcJS/calcScript.js
@@ -0,0 +1,22 @@
+// operator functions
+
+function add(value1, value2){
+    let result = value1 + value2;
+    console.log(result);
+};
+
+function subtract(value1, value2){
+    let result = value1 - value2;
+    console.log(result);
+};
+
+function multipy(value1, value2){
+    let result = value1 * value2;
+    console.log(result);
+};
+
+function divide(value1, value2){
+    let result = value1 / value2;
+    console.log(result);
+};
+
